13a0eab29bb78e1b0031e9b30cee9127437c4b86
sususuj <yimsuji193@gmail.com>
2026-08-09 02:33:33 +0900

    fix: 닉네임 조회에서 탈퇴 사용자를 제외하도록 수정

--- src/main/java/com/server/user/repository/UserRepository.java
+++ src/main/java/com/server/user/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByNickname(String nickname);
+    Optional<User> findByNicknameAndDeletedAtIsNull(String nickname);
 
-    boolean existsByNickname(String nickname);
+    boolean existsByNicknameAndDeletedAtIsNull(String nickname);
 }
