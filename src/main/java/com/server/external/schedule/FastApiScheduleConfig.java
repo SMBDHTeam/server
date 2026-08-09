2ff5dd7f21d0fff5bc148350da79ea6d96f29dc9
MIJUUUUU <dlalwnjenny@naver.com>
2026-08-09 18:35:37 +0900

    refactor: configure FastAPI RestClient with shared Jackson converter

--- src/main/java/com/server/external/schedule/FastApiScheduleConfig.java
+++ src/main/java/com/server/external/schedule/FastApiScheduleConfig.java
@@ -1,7 +1,11 @@
 package com.server.external.schedule;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
@@ -9,14 +13,29 @@ import org.springframework.web.client.RestClient;
 public class FastApiScheduleConfig {
 
     @Bean
-    RestClient fastApiScheduleRestClient(FastApiScheduleProperties properties) {
+    RestClient fastApiScheduleRestClient(
+            FastApiScheduleProperties properties,
+            ObjectMapper objectMapper
+    ) {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setConnectTimeout(properties.connectTimeout());
         requestFactory.setReadTimeout(properties.readTimeout());
 
+        MappingJackson2HttpMessageConverter jacksonConverter =
+                new MappingJackson2HttpMessageConverter(objectMapper);
+
         return RestClient.builder()
                 .baseUrl(properties.baseUrl())
                 .requestFactory(requestFactory)
+                .messageConverters(converters -> prioritizeJacksonConverter(converters, jacksonConverter))
                 .build();
+    }
+
+    private void prioritizeJacksonConverter(
+            List<HttpMessageConverter<?>> converters,
+            MappingJackson2HttpMessageConverter jacksonConverter
+    ) {
+        converters.removeIf(MappingJackson2HttpMessageConverter.class::isInstance);
+        converters.add(0, jacksonConverter);
     }
 }
